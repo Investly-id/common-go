b72367f80ea7f2d4ed71c97f18ad83bb3f188aa4
hasan ikhtiarta <hasan.ikhtiarta@gmail.com>
2022-07-05 15:35:05 +0700

    Feat: FileValidationV2

--- utils/validator.go
+++ utils/validator.go
@@ -104,25 +104,48 @@ func FileValidationV2(c echo.Context, fieldName string, isRequired bool, fileTyp
 		return errors.New(fmt.Sprintf("Kolom %s wajib diisi", fieldName))
 	}
 
+	imageType := []string{".jpg", ".jpeg", ".png"}
+	pdfType := []string{".pdf"}
+
+	// Define allowed formats
+	allowedFormats := make([]string, 0)
+	for _, fileType := range fileTypes {
+		if fileType == FILE_IMAGE {
+			allowedFormats = append(allowedFormats, imageType...)
+		} else if fileType == FILE_PDF {
+			allowedFormats = append(allowedFormats, pdfType...)
+		}
+	}
+
 	if files != nil {
 		for _, i := range files {
 
 			ext := strings.ToLower(path.Ext(i.Filename))
 
-			for _, fileType := range fileTypes {
-				if fileType == FILE_IMAGE {
-					if ext != ".jpg" && ext != ".jpeg" && ext != ".png" {
-						return errors.New("Hanya boleh gambar dengan format (.jpg, .jpeg, .png)")
-					}
+			flag := false
+			for _, allowedFormat := range allowedFormats {
+				if ext == allowedFormat {
+					flag = true
+				}
+			}
 
+			// return if ext doesn't exist in allowed format
+			if !flag {
+				return errors.New(fmt.Sprintf("File hanya boleh dengan format (%s)", strings.Join(allowedFormats, ", ")))
+			}
+
+			// Check image size
+			for _, fileTypeImage := range imageType {
+				if ext == fileTypeImage {
 					if i.Size > MAX_IMAGE_SIZE {
 						return errors.New(fmt.Sprintf("Maksimal ukuran gambar = %d", MAX_IMAGE_SIZE))
 					}
-				} else if fileType == FILE_PDF {
-					if ext != ".pdf" {
-						return errors.New("Hanya boleh dokumen dengan format .pdf")
-					}
+				}
+			}
 
+			// Check pdf size
+			for _, fileTypePdf := range pdfType {
+				if ext == fileTypePdf {
 					if i.Size > MAX_PDF_SIZE {
 						return errors.New(fmt.Sprintf("Maksimal ukuran dokumen = %d", MAX_PDF_SIZE))
 					}
